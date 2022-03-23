6ad23a20c917c16a773dc09c1d2e0f4fa376ab4c
sogrinn <alexsandr99.99.99@gmail.com>
2022-03-23 15:00:32 +0300

    add models

--- gamecritic/gamecritic_site/models.py
+++ gamecritic/gamecritic_site/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    developer = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    game_genre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.IntegerField()
